b20cac71a84e7523dcdc4d044229dd56787bfaf6
clon525 <myagkov-andrey98@mail.ru>
2019-03-22 10:41:33 +0400

    TestNG
    Добавлена поддержка TestNG, удалена поддержка JUnit

--- src/test/java/ru/org/autotest/AutoSettings.java
+++ src/test/java/ru/org/autotest/AutoSettings.java
@@ -1,13 +1,22 @@
 package ru.org.autotest;
-
-import org.junit.After;
-import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutoSettings {
     public  ChromeDriver driver;
-    @Before
+    @BeforeMethod
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Andrew\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -17,7 +26,7 @@ public class AutoSettings {
 
 
 
-    @After
+    @AfterMethod
     public  void tearDown(){
 
     }
--- src/test/java/ru/org/autotest/FirstTest.java
+++ src/test/java/ru/org/autotest/FirstTest.java
@@ -1,7 +1,17 @@
 package ru.org.autotest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,7 +30,7 @@ public class FirstTest extends AutoSettings {
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 //проверка соответствия названия заголовка
         (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.header2-nav__user")));
-        Assert.assertTrue("Кнопка входа профиля не найдена", driver.findElement(By.cssSelector("div.header2-nav__user")).getText().equals("Мой профиль"));
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.header2-nav__user")).getText().equals("Мой профиль"));
     }
 
 }
